2be83b511e1584d65b458ad51ffebea126943eb8
EmilyRoldan <eroldan@uninorte.edu.co>
2025-03-30 21:47:32 -0500

    adding google fonts, padding and textstyle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:f_web_service_random_user_template/presentation/pages/startpage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Conference Management App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        //fontFamily: 'LeagueSpartan',
+        textTheme: GoogleFonts.leagueSpartanTextTheme(),
       ),
       home: const Startpage(),
     );

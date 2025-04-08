269308b5496da0ee475ad4e805b67a66c5d89699
Ana María Ardila Pacheco <anaardila@uninorte.edu.co>
2025-04-07 21:39:39 -0500

    fixing bottom navbar

--- lib/presentation/widgets/bottom_nav_bar.dart
+++ lib/presentation/widgets/bottom_nav_bar.dart
@@ -3,33 +3,35 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class BottomNavBar extends StatelessWidget {
-  final BottomNavController bottomNavController =
-      Get.put(BottomNavController());
+  final BottomNavController bottomNavController = Get.find<BottomNavController>();
 
   BottomNavBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => BottomNavigationBar(
-          currentIndex: bottomNavController.currentIndex.value,
-          onTap: (index) => bottomNavController.onTap(index),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.event),
-              label: 'Events',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'My Events',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          selectedItemColor: Colors.purple,
-          unselectedItemColor: Colors.grey,
-          showUnselectedLabels: true,
-        ));
+    // Obtenemos el índice actual basado en la ruta
+    final currentIndex = bottomNavController.getCurrentIndex();
+    
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) => bottomNavController.onTap(index),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.event),
+          label: 'Events',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          label: 'My Events',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      selectedItemColor: Colors.purple,
+      unselectedItemColor: Colors.grey,
+      showUnselectedLabels: true,
+    );
   }
 }

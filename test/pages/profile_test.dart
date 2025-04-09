b929f73075ba0c8b78939e8c3c8e4e912d61c647
Ana María Ardila Pacheco <anaardila@uninorte.edu.co>
2025-04-09 08:57:23 -0500

    test

--- test/pages/profile_test.dart
+++ test/pages/profile_test.dart
@@ -5,18 +5,18 @@ import 'package:get/get.dart';
 import 'package:f_project_1/presentation/pages/profile.dart';
 import 'package:f_project_1/presentation/controllers/home_controller.dart';
 import 'package:f_project_1/presentation/controllers/event_controller.dart';
+import 'package:f_project_1/presentation/controllers/bottom_nav_controller.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
 
 // Test controller for Home
 class TestHomeController extends HomeController {
   @override
-  // ignore: overridden_fields
   final RxString name = 'Test User'.obs;
 }
+
 // Test controller for Events
 class TestEventController extends EventController {
   @override
-  // ignore: overridden_fields
   final RxList<EventModel> joinedEvents = <EventModel>[].obs;
 
   void addTestEvents(List<EventModel> events) {
@@ -30,20 +30,18 @@ void main() {
 
   setUp(() {
     Get.reset();
-
     testHomeController = TestHomeController();
     testEventController = TestEventController();
-
     Get.put<HomeController>(testHomeController);
     Get.put<EventController>(testEventController);
+    Get.put(BottomNavController());
   });
 
   tearDown(() {
     Get.reset();
   });
 
-  Widget buildTestableWidget(Widget child,
-      {AdaptiveThemeMode initialMode = AdaptiveThemeMode.dark}) {
+  Widget buildTestableWidget(Widget child, {AdaptiveThemeMode initialMode = AdaptiveThemeMode.dark}) {
     return AdaptiveTheme(
       light: ThemeData.light(),
       dark: ThemeData.dark(),
@@ -58,11 +56,13 @@ void main() {
 
   testWidgets('Displays user name correctly', (tester) async {
     await tester.pumpWidget(buildTestableWidget(MyProfile()));
+    await tester.pumpAndSettle();
     expect(find.text('Test User'), findsOneWidget);
   });
 
   testWidgets('Displays event counter correctly (0 events)', (tester) async {
     await tester.pumpWidget(buildTestableWidget(MyProfile()));
+    await tester.pumpAndSettle();
     expect(find.text('You have subscribed to 0 events!'), findsOneWidget);
   });
 
@@ -70,59 +70,62 @@ void main() {
     testEventController.joinedEvents.addAll([
       EventModel(
         id: 1,
-        title: 'Test Event 1',
-        location: 'Location 1',
-        details: 'Details',
-        availableSpots: 10,
-        participants: 20,
-        date: '2023-06-01',
+        title: 'Event 1',
+        location: 'Place 1',
+        details: 'Details 1',
+        participants: 10,
+        availableSpots: 5,
+        date: 'April 20, 2025, 9:00 AM',
         path: 'test',
-        type: 'Test',
-        isJoined: true,
+        type: 'test',
       ),
       EventModel(
         id: 2,
-        title: 'Test Event 2',
-        location: 'Location 2',
-        details: 'Details',
-        availableSpots: 8,
-        participants: 10,
-        date: '2023-06-02',
+        title: 'Event 2',
+        location: 'Place 2',
+        details: 'Details 2',
+        participants: 12,
+        availableSpots: 3,
+        date: 'April 21, 2025, 10:00 AM',
         path: 'test',
-        type: 'Test',
-        isJoined: true,
+        type: 'test',
       ),
     ]);
 
     await tester.pumpWidget(buildTestableWidget(MyProfile()));
+    await tester.pumpAndSettle();
     expect(find.text('You have subscribed to 2 events!'), findsOneWidget);
   });
 
   testWidgets('Dark mode switch reflects the current theme', (tester) async {
-    await tester.pumpWidget(buildTestableWidget(MyProfile()));
+    await tester.pumpWidget(buildTestableWidget(MyProfile(), initialMode: AdaptiveThemeMode.dark));
+    await tester.pumpAndSettle();
     final switchWidget = tester.widget<Switch>(find.byType(Switch));
     expect(switchWidget.value, true);
   });
 
   testWidgets('Toggles theme when switch is tapped', (tester) async {
-    await tester.pumpWidget(buildTestableWidget(MyProfile()));
+    await tester.pumpWidget(buildTestableWidget(MyProfile(), initialMode: AdaptiveThemeMode.dark));
+    await tester.pumpAndSettle();
     final switchFinder = find.byType(Switch);
     await tester.tap(switchFinder);
-    await tester.pump();
+    await tester.pumpAndSettle();
     expect(tester.widget<Switch>(switchFinder).value, false);
   });
 
   testWidgets('Navigates to home when "Change name" is pressed', (tester) async {
     await tester.pumpWidget(
-      buildTestableWidget(
-        GetMaterialApp(
+      AdaptiveTheme(
+        light: ThemeData.light(),
+        dark: ThemeData.dark(),
+        initial: AdaptiveThemeMode.dark,
+        builder: (light, dark) => GetMaterialApp(
+          theme: light,
+          darkTheme: dark,
           initialRoute: '/profile',
           getPages: [
             GetPage(name: '/profile', page: () => MyProfile()),
-            GetPage(
-              name: '/home',
-              page: () => const Scaffold(body: Center(child: Text('Home Screen'))),
-            )
+            GetPage(name: '/home', page: () => const Scaffold(body: Text('Home Screen'))),
           ],
         ),
       ),
@@ -130,7 +133,6 @@ void main() {
 
     await tester.tap(find.byKey(const Key('changeNameTile')));
     await tester.pumpAndSettle();
-
     expect(find.text('Home Screen'), findsOneWidget);
   });
 }
